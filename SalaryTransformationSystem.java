1c08d19095e7e11285bc3a775ba23a27b6661e27
vtu28652-code <vtu28652@veltech.edu.in>
2026-08-12 10:55:58 +0530

    SalaryTransformationSystem.java

---
+++ SalaryTransformationSystem.java
@@ -0,0 +1,21 @@
+public class Task {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> salaries = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            salaries.add(sc.nextInt());
+        }
+        salaries.stream()
+                .map(salary -> salary + (salary * 10 / 100))
+                .forEach(salary -> System.out.print(salary + " "));
+    }
+}
+## Salary Transformation
+  
+### Input
+5
+1000 2000 3000 4000 5000
+
+### Output
+1100 2200 3300 4400 5500
