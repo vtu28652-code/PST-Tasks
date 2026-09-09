300eb82c3d1e272be2fd50ff36b0b94ea55de160
vtu28652-code <vtu28652@veltech.edu.in>
2026-09-09 09:55:12 +0530

    Create Build Array from Permutation.java

---
+++ Week-2/Build Array from Permutation.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
+#Input
+nums = [0,2,1,5,3,4]
+Output
+[0,1,2,4,5,3]
