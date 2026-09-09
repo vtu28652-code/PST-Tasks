ba8b386827354df054ec25146d5fe1f056277c41
vtu28652-code <vtu28652@veltech.edu.in>
2026-09-09 10:31:23 +0530

    Create Day of the Year.java

---
+++ Week-3/Day of the Year.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+class Solution {
+    public int dayOfYear(String date) {
+
+        LocalDate d = LocalDate.parse(date);
+
+        return d.getDayOfYear();
+    }
+}
+#Input 
+  date = "2019-01-09"
+  
+#Output
+9
